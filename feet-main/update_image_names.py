7630beae65fd9287e3b296386f87fb96ac10df75
HP <HP@loop-depaulsecure-240-16.depaulsecure-student.depaul.edu>
2024-04-11 10:50:36 -0500

    populating healthy images to dataset

--- feet-main/update_image_names.py
+++ feet-main/update_image_names.py
@@ -6,7 +6,7 @@ from path import my_path
 
 df = pd.read_csv(os.path.join(my_path, 'json_data', 'sag_T1_json_columns', 'annotations', 'results_box_cleaned_calculation.csv'))
   
-
+# use thsi fucntion to populate healthy image dataset folder
 def get_png_full_path(file):
   """
   Finds the full path of a PNG file with the given filename.
@@ -17,18 +17,20 @@ def get_png_full_path(file):
   Returns:
       str: Full path of the file if found, None otherwise.
   """
+  
   current_dir = my_path
   count = 0
   for root, dirs, files in os.walk(current_dir):
       if files:
           for filename in files:
-              if filename.lower().endswith(('.jpg', '.jpeg', '.png')) and filename != file:
+              if filename.lower().endswith(('.jpg', '.jpeg', '.png')) and filename == file:
                   count +=1 
                   
                   image_path = os.path.join(root, filename)
                   #return image_path
-  print(count)
+  return count
   #return None
+  '''
 def move_images_to_healthy_folder(my_path, df):
     #/Users/HP/src/feet_fracture_data/864/validate
     healthy_folder = os.path.join(my_path, '864', 'validate',"healthy")
@@ -46,8 +48,9 @@ def move_images_to_healthy_folder(my_path, df):
                     #print(dest_path)
                     shutil.move(src_path, dest_path)
                     print(f"Moved {filename} to healthy folder.")
-
-move_images_to_healthy_folder(my_path, df)                  #
+'''
+print(get_png_full_path("P001 SAGT1_002.jpg"))
+#move_images_to_healthy_folder(my_path, df)                  #
 #print(df.head())
 #df.to_csv(os.path.join(my_path, 'json_data', 'sag_T1_json_columns', 'annotations', 'results_box_cleaned_calculation.csv'), index=False)
 
