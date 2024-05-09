7cd47e807e1a045fc5aa3105b75b2afeab6f84c9
HP <HP@loop-225-34.loop-wireless.depaul.edu>
2024-05-09 14:50:30 -0500

    unzip dataset

--- feet-main/main.py
+++ feet-main/main.py
@@ -1,7 +1,10 @@
-import sys
-from zipfile import PyZipFile
 
-zip_file = '/home/odange/repo/feet-main/864.zip'
-print(f'Extracting {zip_file}...')
-pzf = PyZipFile(zip_file)
-pzf.extractall()
+import zipfile
+
+# zip_file = '/home/odange/repo/feet-main/864.zip'
+zip_file = '/home/odange/repo/feet-main'
+
+with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+    zip_ref.extractall(zip_file)
+    print("The file was extracted successfully")
+    
